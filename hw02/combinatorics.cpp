0ec4bfa3a73de51ed0af2625370d5a678e249e3a
Junjie Wang <junjie.wang@tum.de>
2023-10-28 19:10:39 +0200

    hw02_update

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -3,17 +3,24 @@
 
 uint64_t factorial(uint64_t val) {
     // TODO
-    return 0;
+    if (val == 0 || val == 1) {
+        return 1;
+    }
+    uint64_t result = 1;
+    for (uint64_t i = 2; i <= val; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    return factorial(val) / (factorial(val - val2));
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    return factorial(val) / (factorial(val2) * (factorial(val - val2)));
 }
--- hw02/run.cpp
+++ hw02/run.cpp
@@ -7,6 +7,5 @@
 int main() {
     // here you can test your functions
     std::cout << "perm(10, 3)=" << permutation(10, 3) << std::endl;
-
     return 0;
 }
