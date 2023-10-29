d61b074897d3f5eb94f1cd4139cba13a8117d53b
Junjie Wang <junjie.wang@tum.de>
2023-10-29 23:51:26 +0100

    hw02_update

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -16,11 +16,17 @@ uint64_t factorial(uint64_t val) {
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
     // TODO
+    if (val < val2){
+        return 0;
+    }
     return factorial(val) / factorial(val - val2);
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
     // TODO
+    if (val < val2){
+        return 0;
+    }
     return factorial(val) / (factorial(val2) * factorial(val - val2));
 }
